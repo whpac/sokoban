eeb5c1020cb168edf77ef285c3f6054291e235ba
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-12-05 15:44:20 +0100

    User can now pick the level to load

--- src/main/java/pl/whpac/sokoban/Sokoban.java
+++ src/main/java/pl/whpac/sokoban/Sokoban.java
@@ -7,15 +7,20 @@ import pl.whpac.sokoban.board.BoardBuilder;
 import pl.whpac.sokoban.board.FileBoardBuilder;
 import pl.whpac.sokoban.display.BoardDisplay;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class Sokoban extends Application {
+    private static String levelPath;
+
     @Override
     public void start(Stage stage) {
         Board board;
         try{
-            InputStream input_stream = getClass().getResourceAsStream("board/lvl1.txt");
+            InputStream input_stream = getLevelStream();
             BoardBuilder board_builder = new FileBoardBuilder(input_stream);
             board = board_builder.createBoard();
         }catch(IOException e){
@@ -29,6 +34,17 @@ public class Sokoban extends Application {
     }
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Type the level name: ");
+        levelPath = s.nextLine();
         launch();
+    }
+
+    private InputStream getLevelStream() throws FileNotFoundException {
+        if(levelPath.startsWith("@")){
+            return getClass().getResourceAsStream("board/" + levelPath.substring(1) + ".txt");
+        }else{
+            return new FileInputStream(levelPath);
+        }
     }
 }
